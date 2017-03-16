32983b2c8348c629433a8f8f38ae0369d17c4c41
krabradosty <krabradosty@gmail.com>
2017-03-16 22:54:30 +0300

    ADDED `cross sections` module ADDED `total cross section coherent neutrino nucleus scattering` function ADDED `physics constants` module

---
+++ src/cross_sections.py
@@ -0,0 +1,23 @@
+import math
+from src.phys_constants import *
+
+
+def total_coherent_neutrino_nucleus(Z, N, nu_energy):
+
+    """
+
+    neutrino + nucleus --> neutrino + nucleus
+
+    :param Z: number of protons in nucleus
+    :param N: number of neutron in nucleus
+    :param nu_energy: initial neutrino energy
+
+    :return: total cross section of the process. See "Sterile neutrinos, coherent scattering, and oscillometry measurements
+    with low-temperature bolometers" by Joseph A. Formaggio and etc (PHYSICAL REVIEW D 85, 013009 (2012))
+
+    """
+
+    weak_charge = N - Z * (1 - 4 * MIXING_ANGLE)
+    cross_section = (G_FERMI * weak_charge * nu_energy)**2 / (4 * math.pi)
+
+    return cross_section
---
+++ src/phys_constants.py
@@ -0,0 +1,5 @@
+import math
+
+G_FERMI = 1.16637876 * 10**(-5) * (10**3)**(-2)   # (MeV)^(-2)
+CONVERSION_hc = 197.326971844 * 10**(-15)  # MeV*m
+MIXING_ANGLE = 0.2311612  # (sin(\theta_w))^2
