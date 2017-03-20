5da31a4b4f24599390150c3cee542eb88059a2ec
krabradosty <krabradosty@gmail.com>
2017-03-20 23:38:46 +0300

    ADDED plot for differential cross section for CN reaction

--- src/phys_constants.py
+++ src/phys_constants.py
@@ -3,3 +3,4 @@ import math
 G_FERMI = 1.16637876 * 10**(-5) * (10**3)**(-2)   # (MeV)^(-2)
 CONVERSION_hc = 197.326971844 * 10**(-15)  # MeV*m
 MIXING_ANGLE = 0.2311612  # (sin(\theta_w))^2
+ATOMIC_MASS_UNIT = 0.931494095457 * 10**3  # MeV
